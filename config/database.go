a8183fad35b164a8d436e7bb7a39692310b549c3
farishuwaidi <farishuwaidi51@gmail.com>
2025-06-19 15:28:29 +0700

    stup load db

--- config/database.go
+++ config/database.go
@@ -11,7 +11,7 @@ var DB *gorm.DB
 
 func LoadDB() {
 
-	connectionStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=Asia/Jakarta", ENV.DB_HOST, ENV.DB_PORT, ENV.DB_USER, ENV.DB_PASSWORD, ENV.DB_NAME)
+	connectionStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=UTC", ENV.DB_HOST, ENV.DB_PORT, ENV.DB_USER, ENV.DB_PASSWORD, ENV.DB_NAME)
 	db, err := gorm.Open(postgres.Open(connectionStr), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
