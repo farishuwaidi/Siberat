91912f96380794dc8dac5ee93122ef0a3b6a0f36
farishuwaidi <farishuwaidi51@gmail.com>
2025-06-20 08:42:53 +0700

    fitur login

--- handler/auth_handler.go
+++ handler/auth_handler.go
@@ -39,3 +39,27 @@ func (h *authHandler) Register(c *gin.Context) {
 
 	c.JSON(http.StatusCreated, res)
 }
+
+func (h *authHandler) Login(c *gin.Context) {
+	var login dto.LoginRequest
+
+	err := c.ShouldBindJSON(&login)
+	if err != nil {
+		errorhandler.HandlerError(c, &errorhandler.BadRequestError{Message: err.Error()})
+		return
+	}
+
+	result, err := h.service.Login(&login)
+	if err !=  nil {
+		errorhandler.HandlerError(c,err)
+		return
+	}
+
+	res := helper.Response(dto.ResponseParams{
+		StatusCode: http.StatusOK,
+		Message: "Login success",
+		Data: result,
+	})
+
+	c.JSON(http.StatusOK, res)
+}
